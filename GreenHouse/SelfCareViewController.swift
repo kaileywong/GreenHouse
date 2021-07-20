0f88b45b8b1d09c682c654a736597deb86a06de6
Kailey Wong <kaileyswong@gmail.com>
2021-07-20 14:50:58 -0700

    Completed menu page, started self care page and items

---
+++ GreenHouse/SelfCareViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelfCareViewController.swift
+//  GreenHouse
+//
+//  Created by Kailey Wong on 7/20/21.
+//
+
+import UIKit
+
+class SelfCareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
